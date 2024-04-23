5f236ea3acf5d0f598bcf549ffad8f17ea6f2a3e
kristofhetenyi <kristofhetenyi228@gmail.com>
2024-04-23 17:59:38 +0200

    Create MetaLabels

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import os
 from irsdk import IRSDK
 from meters.registry import registry, get_job_name
 from meters.client_data import client_last_write
-from prometheus_client import push_to_gateway
+from prometheus_client import push_to_gateway, Gauge
+import telemetry
 
 PUSH_GW_URL = os.getenv('PUSH_GW_URL', 'http://localhost:9091')
 MODE_BLACKLIST = 1
@@ -24,6 +25,11 @@ def main():
     ir = IRSDK()
     ir.startup("../test_files/dump.txt")
     ir.freeze_var_buffer_latest()
+    metaLabels = telemetry.Labels(telemetry.META_LABELS)
+    print(metaLabels.get_keys())
+    print(metaLabels.get_values(ir))
+    laptime = Gauge('laptime', 'Laptime', metaLabels.get_keys(), registry=registry)
+    laptime.labels(*metaLabels.get_values(ir)).set(ir['LapLastLapTime'])
     write_telemetry("SessionTime", ir['SessionTime'])
 
 
--- src/telemetry.py
+++ src/telemetry.py
@@ -1,5 +1,42 @@
+from irsdk import IRSDK
+from prometheus_client import Gauge, Counter
+from meters.registry import registry
+
 META_LABELS = {
-    "": "",
+    "WeekendInfo": {
+        "TrackID": "track_id",
+        "SeriesID": "series_id",
+        "SeasonID": "season_id",
+        "SessionID": "session_id",
+        "SubSessionID": "subsession_id",
+    },
 }
 
 
+class Labels:
+    def __init__(self, labels: dict):
+        self.labels = labels
+
+    def _get_keys(self, d):
+        ks = []
+        for key, value in d.items():
+            if type(value) is dict:
+                ks.extend(self._get_keys(value))
+            else:
+                ks.append(value)
+        return ks
+
+    def get_keys(self):
+        return self._get_keys(self.labels)
+
+    def _get_values(self, d, ir):
+        vs = []
+        for key, value in d.items():
+            if type(value) is dict:
+                vs.extend(self._get_values(value, ir[key]))
+            else:
+                vs.append(ir[key])
+        return vs
+
+    def get_values(self, ir):
+        return self._get_values(self.labels, ir)
