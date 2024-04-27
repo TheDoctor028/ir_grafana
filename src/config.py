834d6fd85d94f8c3cf3e8e79c78480b595bd89d6
kristofhetenyi <kristofhetenyi228@gmail.com>
2024-04-27 15:46:36 +0200

    Add config file error handling

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 
-CONFIG_FILE = os.getenv('CONFIG_FILE', 'config.yaml')
+CONFIG_FILE = os.getenv('CONFIG_FILE', f'{os.getcwd()}//config.yaml')
 
 
 class Config:
@@ -16,9 +16,15 @@ class Config:
         return self._config[item]
 
     def load(self):
-        with open(CONFIG_FILE, 'r') as file:
-            self._config = yaml.safe_load(file)
-        pass
+        try:
+            with open(CONFIG_FILE, 'r') as file:
+                self._config = yaml.safe_load(file)
+        except Exception as e:
+            print(f"Error loading config: {e}\n" +
+                  f"Make sure you have a config.yaml file in the working directory ({os.getcwd()})." +
+                  f"Or set the CONFIG_FILE environment variable to the path of the config file."
+                  )
+            exit(1)
 
     def job_name(self):
         return 'iracing_telemetry_client_' + self._config['client_id']
